a90619bd50089082439c765558e61f64497ede00
Mario Bonilla <mario.bonilla@reale.cl>
2019-08-01 00:50:06 -0400

    clase 17 finalizada

---
+++ 02-fundamentos/template-string.js
@@ -0,0 +1,10 @@
+let nombre = 'Deadpool';
+let real = 'Wade Wilson';
+
+console.log(`${nombre } ${real}`);
+
+function getNombre(){
+    return `${nombre } es ${real}`;
+}
+
+console.log(`Resultado funcion: ${getNombre()}`);
