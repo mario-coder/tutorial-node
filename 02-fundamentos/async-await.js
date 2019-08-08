7de9dadcd4f3c5aa57c690857e1f475a9547f6f5
Mario Bonilla <mario.bonilla@reale.cl>
2019-08-08 13:53:02 -0400

    Clase 23 (async await) finalizada

--- 02-fundamentos/async-await.js
+++ 02-fundamentos/async-await.js
@@ -3,11 +3,40 @@
  */
 
 
-let getNombre = async() => {
-    return 'Mario';
+// let getNombre = async() => {
+
+//     //undefined.nombre;
+//     throw new Error('No existe el nombre');
+
+//     return 'Mario';
+// }
+
+
+let getNombre = () => {
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            resolve('Mario');
+        }, 3000);
+
+    });
 }
 
 
+let saludo = async() => {
+    let nombre = await getNombre();
+
+    return `Hola ${nombre}`;
+}
 
 
-console.log(getNombre());
+// getNombre().then(nombre => {
+//     console.log(getNombre());
+// }).catch( e => {
+//     console.log('Error de Async', e);
+// });
+
+
+saludo().then(mensaje => {
+    console.log(mensaje);
+});
