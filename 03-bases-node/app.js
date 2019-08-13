321f252b3d654d546d5d525c3995bfea36ba9d12
Mario Bonilla <mario.bonilla@reale.cl>
2019-08-13 13:24:29 -0400

    Clase 31 (parametros linea de comando) finalizada

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -2,7 +2,15 @@
 
 const { crearArchivo } = require('./multiplicar/multiplicar');
 
-let base = 10;
+//let base = 10;
+
+//console.log(process.argv);
+let argv = process.argv;
+let parametro = argv[2];
+
+let base = parametro.split('=')[1];
+
+console.log(base);
 
 
 
