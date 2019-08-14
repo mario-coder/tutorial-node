3503dc12b5677e54e82f694b3c3826618e96366b
Mario Bonilla <mario.bonilla@reale.cl>
2019-08-14 13:59:19 -0400

    Clase 35 (optimizacion yargs) finalizada

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,28 +1,5 @@
 //requireds
-const argv = require('yargs')
-    .command('listar', 'Imprime en consola la tabla de multiplicar', {
-        base: {
-            demand: true,
-            alias: 'b'
-        },
-        limite: {
-            alias: 'l',
-            default: 10
-        }
-    })
-    .command('crear', 'Genera una tabla de multiplicar en un archivo txt', {
-        base: {
-            demand: true,
-            alias: 'b'
-        },
-        limite: {
-            alias: 'l',
-            default: 10
-        }
-    })
-    .help()
-    .argv;
-
+const argv = require('./config/yargs').argv;
 const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
 
 let comando = argv._[0];
