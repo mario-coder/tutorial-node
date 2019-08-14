f0ca0a243c4b03d157512e1da144d10eec1244ca
mario <mariobonillam@gmail.com>
2019-08-14 01:00:35 -0400

    Clase 33 (yargs) finalizada

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,19 +1,32 @@
 //requireds
+const argv = require('yargs')
+    .command('listar', 'Imprime en consola la tabla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        }
+    })
+    .help()
+    .argv;
 
 const { crearArchivo } = require('./multiplicar/multiplicar');
 
 //let base = 10;
 
 //console.log(process.argv);
-let argv = process.argv;
-let parametro = argv[2];
+let argv2 = process.argv;
+//let parametro = argv[2];
+//let base = parametro.split('=')[1];
 
-let base = parametro.split('=')[1];
-
-console.log(base);
-
+console.log('base: ' + argv.base);
+console.log('limite: ' + argv.limite);
 
 
-crearArchivo(base)
+
+crearArchivo(argv.base)
     .then(archivo => console.log(`Archivo creado: ${archivo}`))
     .catch(e => console.log(e));
