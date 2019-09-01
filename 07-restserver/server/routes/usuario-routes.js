2679bd3a9da8b5e408e556e8b8c298bb41e8eb7e
Mario Bonilla <mariobonillam@gmail.com>
2019-09-01 00:06:53 -0400

    Clase 98 - PUT

--- 07-restserver/server/routes/usuario-routes.js
+++ 07-restserver/server/routes/usuario-routes.js
@@ -62,10 +62,24 @@ app.post('/usuario', (req, res) => {
 app.put('/usuario/:id', (req, res) => {
 
     let id = req.params.id;
+    let body = req.body;
 
-    res.json({
-        id
+    Usuario.findByIdAndUpdate(id, body, { new: true }, (err, usuarioDB) => {
+
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
     });
+
 });
 
 
