2c227197e49f209c3698d18b539ac497e2c1eb6d
Mario Bonilla <mario.bonilla@reale.cl>
2019-09-10 14:04:26 -0300

    Clase 100 - resultados paginados GET

--- 07-restserver/server/routes/usuario-routes.js
+++ 07-restserver/server/routes/usuario-routes.js
@@ -10,7 +10,31 @@ const app = express();
 //METODOS HTTP
 //obtener registros
 app.get('/usuario', (req, res) => {
-    res.send('get Usuario');
+    //res.send('get Usuario');
+
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+
+    Usuario.find({})
+            .skip(desde)
+            .limit(limite)
+            .exec((err, usuarios) => {
+                if (err) {
+                    return res.status(400).json({
+                        ok: false,
+                        err
+                    });
+                }
+
+                res.json({
+                    ok: true,
+                    usuarios
+                });
+            });
+
 });
 
 //crear registro
